6d3fa1fd1b4b86f7e746275c4bac7b04682bc148
taku0625 <nakaotakumi0625@gmail.com>
2025-03-02 19:35:42 +0900

    clean: build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME="mast3r-slam"
-TAG="latest"
-
-# DOCKER_BUILDKIT=0 docker build --target deploy -f Dockerfile -t ${IMAGE_NAME}:${TAG} ../
-DOCKER_BUILDKIT=0 docker compose build
-
-echo "Docker image ${IMAGE_NAME}:${TAG} built successfully."
+DOCKER_BUILDKIT=0 docker compose build --progress plain
