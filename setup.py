6ef5dba91a9711bd31aad82f1b80d58e7f20fe67
Masahiro Ogawa <m-ogawa@sensyn-robotics.com>
2025-07-22 15:40:06 +0900

    fix no module by reading requirements.txt

--- setup.py
+++ setup.py
@@ -46,16 +46,13 @@ if has_cuda:
 else:
     print("CUDA not found, cannot compile backend!")
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="mast3r_slam",
     version="0.1.0",
-    install_requires=[
-        "opencv-python",
-        "tqdm",
-        "pyyaml",
-        "natsort",
-        "lietor",
-    ],
+    install_requires=requirements,
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
 )
